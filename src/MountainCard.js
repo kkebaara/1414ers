e661355435658e1c6cc1bc611b4308bbde58a9a2
Kenneth Kebaara <kkebaara@yahoo.com>
2022-02-16 00:58:11 -0700

    able to display all mountains

---
+++ src/MountainCard.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+function MountainCard({ id, name, image, elevation, hikingDistance, difficulty, funFact }){
+    
+
+    return (
+        <li className="card_item">
+            <div className="card">
+                <img
+                src={image}
+                alt={name}
+                className="card_image"
+                />
+                <div className="card_content">
+                    <div className="card_title">{name}</div>
+                    <p className="card_detail">{elevation}</p>
+                    <p> {hikingDistance}</p>
+                    <p> {funFact}</p>
+                    <p> {difficulty}</p>
+                </div>
+            </div> 
+        </li>
+    )
+
+}
+
+export default MountainCard
