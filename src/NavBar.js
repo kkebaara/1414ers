4da7211cf3c4a27b115e9b2e494dc0c5651faa9e
Kenneth Kebaara <kkebaara@yahoo.com>
2022-02-17 15:50:03 -0700

    cleaning up

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom"
+
 
 const linkStyles = {
     display: "inline-block",
