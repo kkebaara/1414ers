f5d53ef5a7288e6aa20b7e637222207bcf094181
Kenneth Kebaara <kkebaara@yahoo.com>
2023-08-17 13:31:18 -0600

    udpate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Router } from 'react-router-dom';
 import App from './Components/App';
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
