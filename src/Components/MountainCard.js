ec3941c2dd3946f4c7103aafb7af65b909f638f5
Kenneth Kebaara <kkebaara@yahoo.com>
2023-08-17 12:33:35 -0600

    update

--- src/Components/MountainCard.js
+++ src/Components/MountainCard.js
@@ -1,27 +1,24 @@
-import React from "react"
+import React, { useState } from 'react';
 
-function MountainCard({ 
-    id, 
-    name, 
-    image, 
-    elevation, 
-    hikingDistance, 
-    difficulty, 
-    funFact 
-})
-
-{
-    return (
-        <li className="card">
-            <img src={image} alt={name} />
-            <div className="card_content"></div>
-                <h4>{name}</h4>
-                <p>Hiking Distance: {hikingDistance}</p>
-                <p>Difficulty: {difficulty}</p>
-                <p>Fun Fact! : {funFact} </p>
-                <p>Elevation : {elevation} </p>
-        </li>
-    )
+function MountainCard({
+	name,
+	image,
+	elevation,
+	hikingDistance,
+	difficulty,
+	funFact,
+}) {
+	return (
+		<li className='card'>
+			<img src={image} alt={name} />
+			<div className='card_content'></div>
+			<h4>{name}</h4>
+			<p>Hiking Distance: {hikingDistance}</p>
+			<p>Difficulty: {difficulty}</p>
+			<p>Fun Fact! : {funFact} </p>
+			<p>Elevation : {elevation} </p>
+		</li>
+	);
 }
 
-export default MountainCard
+export default MountainCard;
