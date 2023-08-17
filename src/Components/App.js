ab2f3d99883541bc7611a9d7c4c082676aa2fa11
Kenneth Kebaara <kkebaara@yahoo.com>
2023-08-17 12:27:27 -0600

    update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from './Header';
-import PlantPage from './PlantPage';
+import MountainPage from './MountainPagePage';
 
 function App() {
 	return (
 		<div className='app'>
 			<Header />
-			<PlantPage />
+			<MountainPage />
 		</div>
 	);
 }
